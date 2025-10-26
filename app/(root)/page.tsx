078e07452a348485608ff09f0ffbe1b2617787d9
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-26 13:38:56 +0900

    error boundary

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,8 @@
-import Hello from "./components/Hello";
-
 const Home = () => {
   console.log("SSR");
   return (
     <main>
-      <div className="text-5xl underline">
-        ようこそNextjs16の世界へ
-        <Hello />
-      </div>
+      <div className="text-5xl underline">ようこそNextjs16の世界へ</div>
     </main>
   );
 };
