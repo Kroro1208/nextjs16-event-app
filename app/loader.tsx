47a40208a410022b4c0ff8c876fa9237c881e77a
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-26 13:46:09 +0900

    ローディングコンポーネント実装

---
+++ app/loader.tsx
@@ -0,0 +1,11 @@
+import Loading from "./components/Loading";
+
+const loader = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Loading />
+    </div>
+  );
+};
+
+export default loader;
