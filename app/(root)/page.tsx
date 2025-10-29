08d1e5bb0707ee089eeaf94e3fe6ff1213b1e295
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-29 22:51:27 +0900

    フロントで一覧取得API叩いて表示させるまで

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
-import { events } from "@/lib/mockData";
+import { Event } from "@/database/event.model";
 import EventCard from "../components/EventCard";
 import ExploreButton from "../components/ExploreButton";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const Home = () => {
+const Home = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
@@ -14,11 +18,13 @@ const Home = () => {
       <div className="mt-20 space-y-7">
         <h3>Feature Events</h3>
         <ul className="events">
-          {events.map((event) => (
-            <li key={event.title} className="list-none">
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: Event) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
